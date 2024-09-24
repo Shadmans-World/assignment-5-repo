0e27dde867213e61a8e002f1efedee6140fedabb
Shadmans-World <shaon10516045@gmail.com>
2024-09-24 22:16:58 +0600

    Full & Final Commit

--- JS/donate.js
+++ JS/donate.js
@@ -1,62 +1,66 @@
 // card-1
-document.getElementById('noakhali-button').addEventListener('click', function (event) {
+    document.getElementById('noakhali-button').addEventListener('click', function (event) {
     event.preventDefault();
     var donateAmount = getInputFieldByID('noakhali-donate-amount');
     var donatedBalance = getNumberById('noakhali-donate');
     var availableMoney = getNumberById('available-money');
-    var myModal = document.getElementById('my_modal_1'); 
+    var myModal = document.getElementById('my_modal_1');
     var modalContent = document.querySelector('#my_modal_1 .modal-box p');
     var modalH3 = document.querySelector('#my_modal_1 .modal-box h3');
     var modalH2 = document.querySelector('#my_modal_1 .modal-box h2');
     if (donateAmount === null || donateAmount === "" || isNaN(donateAmount) || donateAmount < 0) {
-        modalH3.innerText="";
-        modalH2.innerText="";
+        modalH3.innerText = "";
+        modalH2.innerText = "";
         modalContent.innerHTML = "Wrong! Enter Positive Number";
         myModal.showModal(); // Display the modal with the error message
-        return;   }if (availableMoney >= donateAmount) {
+        return;
+    }
+    if (availableMoney >= donateAmount) {
         availableMoney = availableMoney - donateAmount;
         document.getElementById('available-money').innerText = availableMoney;
         donatedBalance = donatedBalance + donateAmount;
         document.getElementById('noakhali-donate').innerText = donatedBalance;
         // MODAL  
-         modalContent.innerHTML = `You have donated for humankind`;
-         modalH3.innerHTML=`Congrates`;
-         modalH2.innerHTML=`Successful`;
-         myModal.showModal(); 
+        modalContent.innerHTML = `You have donated for humankind`;
+        modalH3.innerHTML = `Congrates`;
+        modalH2.innerHTML = `Successful`;
+        myModal.showModal();
         // History
         var newDiv = document.createElement('div');
         newDiv.classList.add('border-2', 'rounded-md', 'p-5', 'mb-7');
         newDiv.innerHTML = `<p>${donateAmount} Taka is Donated for Flood at Noakhali, Bangladesh</P>
  <p>${new Date()}<p>
- ` 
- document.getElementById('history').appendChild(newDiv);
-    } else {
+ `
+        document.getElementById('history').appendChild(newDiv);
+    }
+     else {
         // Change the content for insufficient balance
-        modalH3.innerText="";
-        modalH2.innerText="";
+        modalH3.innerText = "";
+        modalH2.innerText = "";
         modalContent.innerHTML = "Sorry, insufficient balance.";
         myModal.showModal();
-    }})
+    }
+})
 
 
 // Card 2
-document.getElementById('feni-button').addEventListener('click', function (event) {
+    document.getElementById('feni-button').addEventListener('click', function (event) {
     event.preventDefault();
     var donateAmount = getInputFieldByID('feni-donate-amount');
     var donatedBalance = getNumberById('feni-donate');
 
     var availableMoney = getNumberById('available-money');
-    var myModal = document.getElementById('my_modal_1'); 
+    var myModal = document.getElementById('my_modal_1');
     var modalContent = document.querySelector('#my_modal_1 .modal-box p');
     var modalH3 = document.querySelector('#my_modal_1 .modal-box h3');
     var modalH2 = document.querySelector('#my_modal_1 .modal-box h2');
 
     if (donateAmount === null || donateAmount === "" || isNaN(donateAmount) || donateAmount < 0) {
-        modalH3.innerText="";
-        modalH2.innerText="";
+        modalH3.innerText = "";
+        modalH2.innerText = "";
         modalContent.innerHTML = "Wrong! Enter Positive Number";
 
-        myModal.showModal(); 
+        myModal.showModal();
         return;
     }
     if (availableMoney >= donateAmount) {
@@ -64,13 +68,13 @@ document.getElementById('feni-button').addEventListener('click', function (event
         document.getElementById('available-money').innerText = availableMoney;
         donatedBalance = donatedBalance + donateAmount;
         document.getElementById('feni-donate').innerText = donatedBalance;
-        
- // MODAL
-         
- modalContent.innerHTML = `You have donated for humankind`;
- modalH3.innerHTML=`Congrates`;
-         modalH2.innerHTML=`Successful`;
- myModal.showModal(); 
+
+        // MODAL
+
+        modalContent.innerHTML = `You have donated for humankind`;
+        modalH3.innerHTML = `Congrates`;
+        modalH2.innerHTML = `Successful`;
+        myModal.showModal();
 
 
         //  history
@@ -83,13 +87,14 @@ document.getElementById('feni-button').addEventListener('click', function (event
 
         document.getElementById('history').appendChild(newDiv);
 
-    } else {
-        
-        modalH3.innerText="";
-        modalH2.innerText="";
+    } 
+    else {
+
+        modalH3.innerText = "";
+        modalH2.innerText = "";
         modalContent.innerHTML = "Sorry, insufficient balance.";
 
-        myModal.showModal(); 
+        myModal.showModal();
     }
 
 })
@@ -105,18 +110,18 @@ document.getElementById('quota-button').addEventListener('click', function (even
     var donatedBalance = getNumberById('quota-donate');
 
     var availableMoney = getNumberById('available-money');
-    var myModal = document.getElementById('my_modal_1'); 
-    var modalContent = document.querySelector('#my_modal_1 .modal-box p'); 
+    var myModal = document.getElementById('my_modal_1');
+    var modalContent = document.querySelector('#my_modal_1 .modal-box p');
     var modalH3 = document.querySelector('#my_modal_1 .modal-box h3');
     var modalH2 = document.querySelector('#my_modal_1 .modal-box h2');
-    
+
 
     if (donateAmount === null || donateAmount === "" || isNaN(donateAmount) || donateAmount < 0) {
-        modalH3.innerText="";
-        modalH2.innerText="";
+        modalH3.innerText = "";
+        modalH2.innerText = "";
         modalContent.innerHTML = "Wrong! Enter Positive Number";
 
-        myModal.showModal(); 
+        myModal.showModal();
         return;
     }
     if (availableMoney >= donateAmount) {
@@ -124,33 +129,34 @@ document.getElementById('quota-button').addEventListener('click', function (even
         document.getElementById('available-money').innerText = availableMoney;
         donatedBalance = donatedBalance + donateAmount;
         document.getElementById('quota-donate').innerText = donatedBalance;
-        
-  // MODAL
-         
-  modalContent.innerHTML = `You have donated for humankind`;
-  modalH3.innerHTML=`Congrates`;
-         modalH2.innerHTML=`Successful`;
-  myModal.showModal(); 
+
+        // MODAL
+
+        modalContent.innerHTML = `You have donated for humankind`;
+        modalH3.innerHTML = `Congrates`;
+        modalH2.innerHTML = `Successful`;
+        myModal.showModal();
 
 
         //  history
         var newDiv = document.createElement('div');
         newDiv.classList.add('border-2', 'rounded-md', 'p-5', 'mb-7');
 
-        newDiv.innerHTML = `<p>${donateAmount} Taka is Donated forInjured in the Quota Movement</P>
+        newDiv.innerHTML = `<p>${donateAmount} Taka is Donated for Injured in the Quota Movement</P>
  <p>${new Date()}<p>
  `
 
         document.getElementById('history').appendChild(newDiv);
 
-    } else {
-        
-        modalH3.innerHTML="";
-        modalH2.innerHTML="";
-        
+    } 
+    else {
+
+        modalH3.innerHTML = "";
+        modalH2.innerHTML = "";
+
         modalContent.innerHTML = "Sorry, insufficient balance.";
 
-        myModal.showModal(); 
+        myModal.showModal();
     }
 
 })
--- JS/shared.js
+++ JS/shared.js
@@ -1,9 +1,15 @@
 function getInputFieldByID(id){
     var donateAmount = document.getElementById(id).value;
-    donateAmount =parseFloat(donateAmount);
+    if(isNaN(donateAmount)){
+        return;
+    }
+    else{
+        donateAmount =parseFloat(donateAmount);
+        return donateAmount;
+    }
     
     
-    return donateAmount;
+   
 }
 
 
